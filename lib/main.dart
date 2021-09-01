6c43b950169fd3b8384492ac3cf453185445ace2
enoch-han <62514237+enoch-han@users.noreply.github.com>
2021-09-01 13:21:38 -0700

    connection to firebase completed and related configurations in the index file

--- lib/main.dart
+++ lib/main.dart
@@ -8,36 +8,40 @@ import 'package:offline/pages/contentPage.dart';
 import 'package:offline/routes/pageRoute.dart';
 import 'package:provider/provider.dart';
 import 'package:offline/navigationDrawer/MenuController.dart';
+import './utils/utils.dart';
 
-
-void main() => runApp(new MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initialization;
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
-
- @override
- Widget build(BuildContext context) {
-   return MultiProvider(
-     providers: [
-       ChangeNotifierProvider(
-            create: (context) => MenuController(),
-          ),
-     ],
-     child: new MaterialApp(
-       title: 'Leisure',
-       theme: ThemeData.dark().copyWith(
-        //scaffoldBackgroundColor: bgColor,
-        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme).apply(bodyColor: Colors.white),
-        //canvasColor: secondaryColor,
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => MenuController(),
+        ),
+      ],
+      child: new MaterialApp(
+        title: 'Leisure',
+        theme: ThemeData.dark().copyWith(
+          //scaffoldBackgroundColor: bgColor,
+          textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+              .apply(bodyColor: Colors.white),
+          //canvasColor: secondaryColor,
+        ),
+        home: UserPage(),
+        routes: {
+          //page routes for the drawer items
+          PageRoutes.user: (context) => UserPage(),
+          PageRoutes.content: (context) => Content(),
+          PageRoutes.load: (context) => Upload(),
+          PageRoutes.login: (context) => Login(),
+        },
       ),
-       home: UserPage(),
-       routes:  {
-         //page routes for the drawer items
-         PageRoutes.user: (context) => UserPage(),
-         PageRoutes.content: (context) => Content(),
-         PageRoutes.load: (context) => Upload(),
-         PageRoutes.login: (context) => Login(),
-       },
-     ),
-   );
- }
+    );
+  }
 }
--- lib/pages/uploadPage.dart
+++ lib/pages/uploadPage.dart
@@ -12,7 +12,6 @@ import 'package:offline/pages/header.dart';
 
 import '../constants.dart';
 
-
 // this is used to upload files to firestore is has a formfield to register different items
 class Upload extends StatefulWidget {
   static const String routeName = '/uploadpage';
@@ -23,221 +22,216 @@ class Upload extends StatefulWidget {
 class _UploadState extends State<Upload> {
   String genrevalue = 'Action';
   String typevalue = 'Movie';
-  String rating = '5';
-  var items =  ['Action','Drama','Comedy','Horror','Thriller','SciFi','Romantic','Romantic Comedy','Time Period Drama','Adventure','Fantasy','Animation','Foriegn'];
-  var type = ['Movie','Series','Music']; 
-  var rate = ['0','1','2','3','4','5'];
-   final formKey = GlobalKey<FormState>();
+  String rating = '1';
+  var items = [
+    'Action',
+    'Comedy',
+    'Fantasy',
+    'Horror',
+    'Mystery',
+    'Drama',
+    'Romance',
+    'Thriller',
+    'Romantic comedy',
+    'Action comedy',
+    'Unknown'
+  ];
+  var type = ['Movie', 'Music'];
+  var rate = ['0', '1', '2', '3', '4', '5'];
+  final formKey = GlobalKey<FormState>();
 
   set file(file_Data_Model file) {}
   TextEditingController name = TextEditingController();
 
- @override
- Widget build(BuildContext context) {
-   //var file;
-   return new Scaffold(
-          key: context.read<MenuController>().scaffoldKey,
-       appBar: PreferredSize(
-         preferredSize: Size(MediaQuery.of(context).size.width, 50),
-         child: Header()
-       ),
-       drawer: NavigationDrawer(),
-       body:  SingleChildScrollView(
-        child: Container(
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.all(15),
-            child: Column(
-              children: <Widget>[
-                //container for the file drop widget 
-                Container(
-                  height: 300,
-                  child: DropZoneWidget(
-                    onDroppedFile: (file) => setState(()=> this.file = file) ,
-                  ),
-                ),
-                SizedBox(height: 15),
-                //DroppedFileWidget(file:file ),
-                //the textformfield for the filename
-                
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: defaultPadding,
-                    vertical: defaultPadding / 2,
-                    ),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                    labelText: 'Enter File Name',
-                    ),
-                    controller: name,
-                    validator: (value){
-                          if(value!.isEmpty){
-                              return "Enter Valid Name!";
-                              }else{
-                                return value;
-                                }
-                        },                                      
-                  )
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: defaultPadding,
-                    vertical: defaultPadding / 2,
-                    ),
-                  child:  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Rating:',
-                        overflow: TextOverflow.clip,
-                        //style: const TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(width: 25),
-                      DropdownButton<String>(
-                        
-                        value: rating,
-                        icon: Icon(Icons.keyboard_arrow_down),
-                        items:rate.map((String items) {
-                          return DropdownMenuItem(
-                            value: items,
-                            child: Text(items)
-                            );}).toList(),
-                        onChanged: (newValue){
-                          setState(() {
-                            rating = newValue!;
-                            });
-                        },
-                    ),
+  @override
+  Widget build(BuildContext context) {
+    //var file;
+    return new Scaffold(
+      key: context.read<MenuController>().scaffoldKey,
+      appBar: PreferredSize(
+          preferredSize: Size(MediaQuery.of(context).size.width, 50),
+          child: Header()),
+      drawer: NavigationDrawer(),
+      body: SingleChildScrollView(
+          child: Container(
+        alignment: Alignment.centerLeft,
+        padding: EdgeInsets.all(15),
+        child: Column(
+          children: <Widget>[
+            //container for the file drop widget
+            Container(
+              height: 300,
+              child: DropZoneWidget(
+                onDroppedFile: (file) => setState(() => this.file = file),
+              ),
+            ),
+            SizedBox(height: 15),
+            //DroppedFileWidget(file:file ),
+            //the textformfield for the filename
 
-                    ],
-                  )
-                ),
-                Padding(padding: EdgeInsets.symmetric(
+            Padding(
+                padding: EdgeInsets.symmetric(
                   horizontal: defaultPadding,
-                  vertical: defaultPadding/2,
+                  vertical: defaultPadding / 2,
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Enter File Name',
+                  ),
+                  controller: name,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Enter Valid Name!";
+                    } else {
+                      return value;
+                    }
+                  },
+                )),
+            Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: defaultPadding,
+                  vertical: defaultPadding / 2,
                 ),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Container(
+                    Text(
+                      'Rating:',
+                      overflow: TextOverflow.clip,
+                      //style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(width: 25),
+                    DropdownButton<String>(
+                      value: rating,
+                      icon: Icon(Icons.keyboard_arrow_down),
+                      items: rate.map((String items) {
+                        return DropdownMenuItem(
+                            value: items, child: Text(items));
+                      }).toList(),
+                      onChanged: (newValue) {
+                        setState(() {
+                          rating = newValue!;
+                        });
+                      },
+                    ),
+                  ],
+                )),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: defaultPadding,
+                vertical: defaultPadding / 2,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
                       alignment: Alignment.centerLeft,
                       height: 100,
                       width: 300,
-                      child: DatePickerWidget()
-                    )
-                  ],
-                ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: defaultPadding,
-                    vertical: defaultPadding / 2,
-                    ),
-                  child:  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        'File Type:',
-                        overflow: TextOverflow.clip,
-                        //style: const TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(width: 25),
-                      DropdownButton<String>(
-                        hint: Text('Choose File type'),
-                        value: typevalue,
-                        icon: Icon(Icons.keyboard_arrow_down),
-                        items:type.map((String items) {
-                          return DropdownMenuItem(
-                            value: items,
-                            child: Text(items)
-                            );}).toList(),
-                        onChanged: (newValue){
-                          setState(() {
-                            typevalue = newValue!;
-                            });
-                        },
-                    ),
-
-                    ],
-                  )
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: defaultPadding,
-                    vertical: defaultPadding / 2,
-                    ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Genre:',
-                        //textAlign: TextAlign.center,
-                        overflow: TextOverflow.clip,
-                        //style: const TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(width: 25),
-                    DropdownButton<String>(
-                      hint: Text('Choose Genre'),
-                      value: genrevalue,
-                      icon: Icon(Icons.keyboard_arrow_down),
-                          items:items.map((String items) {
-                            return DropdownMenuItem(
-                              value: items,
-                              child: Text(items)
-                              );}).toList(),
-                      onChanged: (newValue){
-                        setState(() {
-                          genrevalue = newValue!;
-                        });
-                        
-                        },
-                    ),
-                  ],
-                ),
+                      child: DatePickerWidget())
+                ],
               ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: defaultPadding,
-                    vertical: defaultPadding / 2,
-                    ),
-                  child:  TextFormField(
-                    maxLines: 7,
-                    decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Description',
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
+            ),
+            Padding(
+                padding: EdgeInsets.symmetric(
                   horizontal: defaultPadding,
                   vertical: defaultPadding / 2,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'File Type:',
+                      overflow: TextOverflow.clip,
+                      //style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(width: 25),
+                    DropdownButton<String>(
+                      hint: Text('Choose File type'),
+                      value: typevalue,
+                      icon: Icon(Icons.keyboard_arrow_down),
+                      items: type.map((String items) {
+                        return DropdownMenuItem(
+                            value: items, child: Text(items));
+                      }).toList(),
+                      onChanged: (newValue) {
+                        setState(() {
+                          typevalue = newValue!;
+                        });
+                      },
+                    ),
+                  ],
+                )),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: defaultPadding,
+                vertical: defaultPadding / 2,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Genre:',
+                    //textAlign: TextAlign.center,
+                    overflow: TextOverflow.clip,
+                    //style: const TextStyle(fontWeight: FontWeight.bold),
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      ElevatedButton(
-                            child: Text('Upload'),
-                            onPressed: (){
-                              if(formKey.currentState!.validate()){                            
-                                Navigator.pushReplacementNamed(context, PageRoutes.user);
-                    }
-                            },
-                            ),
-                    ]
-                  )
-                ),   
-                     
-                      if (!Responsive.isMobile(context))
-                        Padding(
-                          padding:
-                              const EdgeInsets.symmetric(horizontal: defaultPadding / 2),
-                        )
-                    ],
-                  ),    
-            )
-          ),
+                  SizedBox(width: 25),
+                  DropdownButton<String>(
+                    hint: Text('Choose Genre'),
+                    value: genrevalue,
+                    icon: Icon(Icons.keyboard_arrow_down),
+                    items: items.map((String items) {
+                      return DropdownMenuItem(value: items, child: Text(items));
+                    }).toList(),
+                    onChanged: (newValue) {
+                      setState(() {
+                        genrevalue = newValue!;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: defaultPadding,
+                vertical: defaultPadding / 2,
+              ),
+              child: TextFormField(
+                maxLines: 7,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Description',
+                ),
+              ),
+            ),
+            Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: defaultPadding,
+                  vertical: defaultPadding / 2,
+                ),
+                child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                  ElevatedButton(
+                    child: Text('Upload'),
+                    onPressed: () {
+                      //******************************************************************** */
+                      if (formKey.currentState!.validate()) {
+                        Navigator.pushReplacementNamed(
+                            context, PageRoutes.user);
+                      }
+                    },
+                  ),
+                ])),
+
+            if (!Responsive.isMobile(context))
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: defaultPadding / 2),
+              )
+          ],
+        ),
+      )),
     );
- }
-
+  }
 }
-
